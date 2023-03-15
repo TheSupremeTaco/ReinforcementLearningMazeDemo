9a2d93829f25f7ae05ec6c1daff28297c03daae2
TheSupremeTaco <maksymlike@gmail.com>
2023-03-15 15:08:56 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Reinforcement Learning Project 1
+# Maksym Mostovyy Z235236707
+# Reinforcement Learning CAP 6629-001
+
+# Main Things for Maze Program using Reinforcement Learning Algorithms
+
+# Maze Board
+#   Can be changed to any user inputted row by column dimensions (default will be 3x3)
+
+# Algorithm Selection
+#   Selects a learning method based on user input (default is Q learning)
+#       Q-Learning
+# Pseudo Code
+#   
+#       Monte Carlo
+
+
+def print_hi(name):
+    print("test")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
